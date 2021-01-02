f0d80dcc0f471786851a4230844d1e017699842b
NakajimaDaniel <danieln17.contato@outlook.com>
2021-01-02 12:53:34 -0300

    session timer and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
+
+
+  const [seconds, setSeconds] = useState(10);
+  const [minutes, setMinutes] = useState(0);
+  const[stopvar, setStopvar] = useState(false);
+
+  const startStop = ()=>{
+    setStopvar(true)
+    console.log(stopvar)
+    if(stopvar == true)
+    setStopvar(false)
+  }
+
+  useEffect(()=>{
+    const myCount = setInterval(() => 
+    {
+      if (seconds > 0) 
+      {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) 
+        {
+          if (minutes === 0) 
+          {
+            clearInterval(myCount)
+          } 
+          else 
+          {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        } 
+
+    }, 1000)
+          
+    if(stopvar === true)
+    {
+      clearInterval(myCount);
+    }
+
+    return ()=> 
+    {
+      clearInterval(myCount);
+    };
+
+    });
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="title">
+        <p>Pomodoro</p>
+        <p>Timer</p>
+      </div>
+
+      <div className="timer">
+        <h2>
+          {minutes<10 ? `0${minutes}` : minutes }:{seconds<10 ? `0${seconds}` : seconds}
+        </h2>
+
+        <div className="button">
+          <button onClick={startStop}>Start/Stop</button>
+        </div>
+
+      </div>
+      
+      <div className="time-adjust">
+        <p>d</p>
+      </div>
+
     </div>
+    
   );
 }
 
